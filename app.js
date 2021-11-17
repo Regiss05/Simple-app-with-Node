eb7456dd4c98dd132b30ef74856baddadb0a83b9
regiss05 <regiss.steven@gmail.com>
2021-11-17 11:02:17 +0200

    this is the whole crud system

--- app.js
+++ app.js
@@ -26,11 +26,26 @@ app.get('/', (req, res) => {
         <form action="/info/get" method="GET">
             <input type="submit" value="GET">
         </form>
+
         <form action="/info/add" method="POST">
         <label for="add">ADD</label>
         <input type="text" name="add" id="add">
         <input type="submit" value="ADD">
-    </form>
+        </form>
+
+        <form action="/info/delete" method="POST">
+        <label for="delete">DELETE: </label>
+        <input type="text" name="delete" id="delete">
+        <input type="submit" value="DELETE">
+        </form>
+
+        <form action="/info/update" method="POST">
+        <label for="oldValue">Old Value: </label>
+        <input type="text" name="oldValue" id="oldValue">
+        <label for="newValue">New Value: </label>
+        <input type="text" name="newValue" id="newValue">
+        <input type="submit" value="UPDATE"> 
+        </form>
     </body>
     </html>
     `);
@@ -58,7 +73,28 @@ app.post('/info/add', (req, res) => {
     }
 });
 
+app.post('/info/delete', (req, res) => {
+    try {
+        pool.connect(async (error, client, release) => {
+            let resp = await client.query(`DELETE FROM test WHERE name = '${req.body.delete}'`);
+            res.redirect('/info/get');
+        })
+    } catch {
+        console.log(error);
+    }
+});
+
+app.post('/info/update', (req, res) => {
+    try {
+        pool.connect(async (error, client, release) => {
+            let resp = await client.query(`UPDATE test SET name = '${req.body.newValue}' WHERE name = '${req.body.oldValue}'`);
+            res.redirect('/info/get');
+        })
+    } catch {
+        console.log(error);
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server Started on ${port}`);
-
 });
