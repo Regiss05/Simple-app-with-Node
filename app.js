8492e2f97067c7b37bc1d438a25367ff672fc6a0
regiss05 <regiss.steven@gmail.com>
2021-11-17 08:03:47 +0200

    first push

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+let pool = new Pool();
+const port = process.env.PORT;
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="/info/get" method="GET">
+            <input type="submit" value="GET">
+        </form>
+    </body>
+    </html>
+    `);
+});
+
+app.get('/info/get', (req, res) => {
+    try {
+        pool.connect(async (error, client, release) => {
+            let resp = await client.query(`SELECT * FROM test`);
+            res.send(resp.rows);
+        })
+    } catch {
+        console.log(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server Started on ${port}`);
+});
